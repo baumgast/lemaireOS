dd506da5b388a9f88d778ed8ad3c4cc8d81430ab
baumgast <baumsen@gmail.com>
2012-08-17 00:48:47 +0200

    started to implement own fitting
    simulate all the single complexes and calculate all combination of sums
    of the x_i. squared difference between data and the sum is the measure
    for the goodness of fit

---
+++ lemaireFit.R
@@ -0,0 +1,22 @@
+#adjustment of the lemaire model to george`s data,
+#calculate for all possible combinations of complexes (sum of x_i) the squared 
+#difference to the data by applying a nested loop
+#try ER alpha first
+#-------------------------------------------------------------------------------
+
+Time = seq(0,180,5)
+Era = c(0,2,6,11,18,5,1,6,17,33,51,65,65,57,31,14,13,26,60,73,70,58,28,19,23,29,55,72,72,58,39,29,39,51,62,64,61)
+
+plot(Time,Era, type = 'l',
+     xlab = 'Time (min)',
+     ylab = '% of bound promoters',
+     ylim = c(0,100))
+grid()
+points(Time,Era, pch = 19)
+
+m = dim(out)[2] - 1
+t = Time[which(Time > 35)]
+rows = which(out[,1] == Time)
+for (i in 1:m) {
+  
+}
--- lemairePlot.R
+++ lemairePlot.R
@@ -1,5 +1,7 @@
-xlim = c(0,max(times))
-ylim = c(0,12)
+xlim = c(40,max(times))
+ylim = c(0,4)
+
+par(mfrow = c(1,1))
 
 plot.new()
 plot.window(xlim,ylim)
@@ -15,3 +17,39 @@ title(main = '',
 plots = seq(2,100,10)
 matpoints(out[,1],out[,plots], type = 'l', lty = 1)
 legend('topright', as.character(plots - 1), col = 1:length(plots),lty = 1, bty = 'n')
+#------------------------------------------------------------------------------
+#summing certain components x_i
+par(mfrow = c(1,1))
+m = dim(out)[2]-1
+
+m1 = 1+round(m/8)
+m2 = 1+round(m*2/3)
+
+ER1 = apply(out[,m1:m2],1, sum)
+
+plot(out[,1], ER1, type = 'l',
+     xlab = 'Time (min)',
+     ylab = '% of bound promoters')
+#legend('topright', c('ER-alpha',paste('complexes m1 =',m1,'to m2 =',m2)), bty = 'n')
+#-------------------------------------------------------------------------------
+m1 = 1+round(m/8)
+m2 = 1+round(m*3/8)
+
+ER2 = apply(out[,m1:m2],1, sum)
+
+points(out[,1],ER2, type = 'l',col = 'red',
+     xlab = 'Time (min)',
+     ylab = '% of bound promoters')
+#legend('topright', c('ER-alpha',paste('complexes m1 =',m1,'to m2 =',m2)), bty = 'n')
+#-------------------------------------------------------------------------------
+m1 = 1+round(m*3/7)
+m2 = 1+round(m*2/3)
+
+ER3 = apply(out[,m1:m2],1, sum)
+
+points(out[,1],ER3, type = 'l',col = 'blue',
+     xlab = 'Time (min)',
+     ylab = '% of bound promoters')
+#legend('topright', c('ER-alpha',paste('complexes m1 =',m1,'to m2 =',m2)), bty = 'n')
+#-------------------------------------------------------------------------------
+points(out[,1],ER3+ER2, type = 'l', col = 'orange')
