1d2b3b456ccc2988f5e6ffa3aa37e2fd734b8060
baumgast <baumsen@gmail.com>
2012-08-17 02:43:51 +0200

    reproduced the plots from lemaire et al

--- lemairePlot.R
+++ lemairePlot.R
@@ -19,37 +19,90 @@ matpoints(out[,1],out[,plots], type = 'l', lty = 1)
 legend('topright', as.character(plots - 1), col = 1:length(plots),lty = 1, bty = 'n')
 #------------------------------------------------------------------------------
 #summing certain components x_i
-par(mfrow = c(1,1))
+#components:
+total = 69
 m = dim(out)[2]-1
 
-m1 = 1+round(m/8)
-m2 = 1+round(m*2/3)
+m11 = 1+round(7/total*m)
+m12 = 1+round(10/total*m)
+ER1 = apply(out[,m11:m12],1,sum)
 
-ER1 = apply(out[,m1:m2],1, sum)
+m21 = 1+round(17/total*m)
+m22 = 1+round(27/total*m)
+ER2 = apply(out[,m21:m22],1,sum)
 
-plot(out[,1], ER1, type = 'l',
-     xlab = 'Time (min)',
+m31 = 1+round(31/total*m)
+m32 = 1+round(41/total*m)
+ER3 = apply(out[,m31:m32],1,sum)
+
+m41 = 1+round(45/total*m)
+m42 = 1+round(48/total*m)
+ER4 = apply(out[,m41:m42],1,sum)
+ER1.4 = apply(out[,m11:m42],1,sum)
+m51 = 1+round(62/total*m)
+m52 = 1+round(65/total*m)
+ER5 = apply(out[,m51:m52],1,sum)
+ER1.5 = apply(out[,m11:m52],1,sum)
+
+#Pol II
+m11 = 1+round(21/total*m)
+m12 = 1+round(27/total*m)
+Pol1 = apply(out[,m11:m12],1,sum)
+
+m21 = 1+round(34/total*m)
+m22 = 1+round(45/total*m)
+Pol2 = apply(out[,m21:m22],1,sum)
+
+#TRIP1
+m11 = 1+round(45/total*m)
+m12 = 1+round(48/total*m)
+TRIP1 = apply(out[,m11:m12],1,sum)
+
+m21 = 1 + round(58/total*m)
+m22 = 1 + round(62/total*m)
+TRIP2 = apply(out[,m21:m22],1,sum)
+
+#HDAC
+m11 = 1+round(52/total*m)
+m12 = 1+round(62/total*m)
+HDAC = apply(out[,m11:m12],1,sum)
+#------------------------------------------------------------------------------
+par(mfrow = c(1,1))
+
+xlim = range(out[,1])
+ylim = c(0,100)
+
+plot.new()
+plot.window(xlim,ylim)
+axis(1)
+axis(2)
+grid()
+box()
+
+title(main = 'ER-alpha',
+      xlab = 'Time (min)',
+      ylab = '% bound promoters')
+
+points(out[,1],ER1+ER2+ER3+ER4+ER5, type = 'l')
+points(out[,1],ER1,type = 'l', col = 'orangered')
+points(out[,1],ER2,type = 'l',col = 'orangered1')
+points(out[,1],ER3,type = 'l',col = 'orangered2')
+points(out[,1],ER4,type = 'l',col = 'orangered3')
+points(out[,1],ER5,type = 'l',col = 'orangered4')
+points(out[,1],ER1.4, type = 'l', col = 'springgreen')
+points(out[,1],ER1.5, type = 'l', col = 'orange')
+#------------------------------------------------------------------------------
+#Pol II
+plot(out[,1],Pol1, type = 'l', ylim = ylim, col = 'orangered',
+     main = 'Pol II',
+     xlab = 'time (min)',
      ylab = '% of bound promoters')
-#legend('topright', c('ER-alpha',paste('complexes m1 =',m1,'to m2 =',m2)), bty = 'n')
-#-------------------------------------------------------------------------------
-m1 = 1+round(m/8)
-m2 = 1+round(m*3/8)
-
-ER2 = apply(out[,m1:m2],1, sum)
-
-points(out[,1],ER2, type = 'l',col = 'red',
-     xlab = 'Time (min)',
+points(out[,1],Pol2,type = 'l', col = 'orangered1')
+points(out[,1],Pol1+Pol2, type = 'l')
+#------------------------------------------------------------------------------
+plot(out[,1],TRIP1, type = 'l', col = 'orangered', ylim = ylim,
+     main = 'TRIP1',
+     xlab = 'time (min)',
      ylab = '% of bound promoters')
-#legend('topright', c('ER-alpha',paste('complexes m1 =',m1,'to m2 =',m2)), bty = 'n')
-#-------------------------------------------------------------------------------
-m1 = 1+round(m*3/7)
-m2 = 1+round(m*2/3)
-
-ER3 = apply(out[,m1:m2],1, sum)
-
-points(out[,1],ER3, type = 'l',col = 'blue',
-     xlab = 'Time (min)',
-     ylab = '% of bound promoters')
-#legend('topright', c('ER-alpha',paste('complexes m1 =',m1,'to m2 =',m2)), bty = 'n')
-#-------------------------------------------------------------------------------
-points(out[,1],ER3+ER2, type = 'l', col = 'orange')
+points(out[,1],TRIP2, type = 'l', col = 'orangered')
+points(out[,1], TRIP1+TRIP2,type = 'l', col = 'black')
